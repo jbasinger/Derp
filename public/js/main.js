9a3da769333909c5a6071d20f6d15c4a58348fae
Justin Basinger <justin.m.basinger@gmail.com>
2013-12-31 15:06:41 -0500

    Added Login, Log out and basic derptitude.

--- public/js/main.js
+++ public/js/main.js
@@ -32,7 +32,7 @@ function setupLogin(){
 		*/
 		} else if (user) {
 			// user authenticated with Firebase
-			G.user = user;
+			G.user = user.uid;
 			showScene("game");
 			setupGame();
 			//console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
@@ -65,7 +65,9 @@ function setupLogin(){
 			email: email,
 			password: password
 		});
-			
+		
+		G.user = auth.uid;
+		
 	});
 	
 	$("#logout").click(function(){
@@ -85,7 +87,7 @@ function setupLogin(){
 		
 		auth.createUser(email,password,function(error,user){
 			if (!error){
-				G.user = user;
+				G.user = user.uid;
 			} else {
 				$(msgId).text(error.message);
 			}
@@ -112,22 +114,159 @@ function showScene(scene){
 
 function setupGame(){
 
-	var Q = Quintus().include("Sprites, Scenes, Input, 2D, Touch, UI").setup("game_scene").controls().touch();
-
+	var Q = Quintus().include("Sprites, Scenes, Input, 2D, UI").setup("game_scene").controls();
+	var fbPlayers = new Firebase(G.base + "players");
+	
+	Q.gravityX=0;
+	Q.gravityY=0;
+	
+	Q.Sprite.extend("Derp", {
+		init:function(p){
+			this._super(p, {
+				asset: "derp.png",
+				color: "red",
+				x:10,
+				y:10
+			});
+			this.add("2d");
+			
+		}
+	});
+	
   Q.scene("start", function(stage){
+		
+		var myDerp = new Q.Derp();
+		var otherDerps = [];
+		
+		myDerp.add("eventedStepControls");
+		
+		myDerp.on("stepping",this,function(pos){
+			fbPlayers.child(G.user).set(pos);
+		});
+		
+		fbPlayers.on("child_changed",function(data){
+			
+			if (data.name() == G.user)
+				return;
+			
+			var leDerp = _.find(otherDerps,function(d){return d.p.uid==data.name();});
+			var pos = data.val();
+			leDerp.p.x=pos.x;
+			leDerp.p.y=pos.y;
+			
+		});
+		
+		fbPlayers.once("value",function(data){
+			console.log(data.val());
+			_.each(data.val(),function(val, key){
+				
+				if (key == G.user){
+					myDerp.p.x = val.x;
+					myDerp.p.y = val.y;
+					return;
+				} else {
+					var anotherDerp = new Q.Derp();
+					anotherDerp.p.x = val.x;
+					anotherDerp.p.y = val.y;
+					anotherDerp.p.uid = key;
+					otherDerps.push(anotherDerp);
+					stage.insert(anotherDerp);
+				}
+				
+			});
+		});
 		
 		stage.insert(new Q.UI.Button({
 				label: "Log Out",
-				y: 150,
-				x: Q.width/2
+				y: 10,
+				x: Q.width-50
 			}, function() {
 				G.auth.logout();
 				showScene("login");
     }));
-
+		
+		stage.collisionLayer(new Q.TileLayer({dataAsset: "test.tmx", layerIndex: 1, sheet: "tiles", tileW:32, tileH:32 }));
+		stage.insert(new Q.TileLayer({dataAsset: "test.tmx", layerIndex: 0, sheet: "tiles", tileW:32, tileH:32, type: Q.SPRITE_NONE }));
+		
+		stage.insert(myDerp);
+		stage.add("viewport").follow(myDerp);
+		
 	});
 	
-	Q.load("blah.json",function(){
+	Q.load("derp.png, test.tmx, dg_grounds32.png",function(){
+		Q.sheet("tiles","dg_grounds32.png",{tileW:32, tileH:32});
 		Q.stageScene("start");
 	});
+	
+	/* Other components */
+	Q.component("eventedStepControls", {
+
+    added: function() {
+      var p = this.entity.p;
+
+      if(!p.stepDistance) { p.stepDistance = 32; }
+      if(!p.stepDelay) { p.stepDelay = 0.2; }
+
+      p.stepWait = 0;
+      this.entity.on("step",this,"step");
+      this.entity.on("hit", this,"collision");
+    },
+
+    collision: function(col) {
+      var p = this.entity.p;
+
+      if(p.stepping) {
+        p.stepping = false;
+        p.x = p.origX;
+        p.y = p.origY;
+				this.entity.trigger("stepping",{x:p.x,y:p.y});
+      }
+
+    },
+
+    step: function(dt) {
+      var p = this.entity.p,
+          moved = false;
+      p.stepWait -= dt;
+
+      if(p.stepping) {
+        p.x += p.diffX * dt / p.stepDelay;
+        p.y += p.diffY * dt / p.stepDelay;
+				this.entity.trigger("stepping",{x:p.x,y:p.y});
+      }
+
+      if(p.stepWait > 0) { return; }
+      if(p.stepping) {
+        p.x = p.destX;
+        p.y = p.destY;
+      }
+      p.stepping = false;
+
+      p.diffX = 0;
+      p.diffY = 0;
+
+      if(Q.inputs['left']) {
+        p.diffX = -p.stepDistance;
+      } else if(Q.inputs['right']) {
+        p.diffX = p.stepDistance;
+      }
+
+      if(Q.inputs['up']) {
+        p.diffY = -p.stepDistance;
+      } else if(Q.inputs['down']) {
+        p.diffY = p.stepDistance;
+      }
+
+      if(p.diffY || p.diffX ) {
+        p.stepping = true;
+        p.origX = p.x;
+        p.origY = p.y;
+        p.destX = p.x + p.diffX;
+        p.destY = p.y + p.diffY;
+        p.stepWait = p.stepDelay;
+      }
+
+    }
+
+  });
 }
